026b36ff688b5f9a9378bd7cc80f4fb07d4e8b49
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-07-16 21:27:33 +0530

    type coercion in JS

--- variables.js
+++ variables.js
@@ -1,3 +1,15 @@
-var firstName = "John";
-var lastName = 'Doe';
-console.log(firstName, lastName);
+// var firstName = "John";
+// var lastName = 'Doe';
+// console.log(firstName, lastName);
+
+
+// VARIABLE MUTATION and TYPE COERCION
+
+var firstName = "Annie";
+var age = 20;
+
+console.log(firstName + ' is ' + age + ' years old.');
+
+var job = "unemployed"
+var isFemale = true;
+console.log(firstName + ' is ' + job + ' and is female : ' + isFemale);
