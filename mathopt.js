765c6130e70fec29449dd5ca8e0ab47b0c8cdb7a
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-07-17 12:20:22 +0530

    basic mathematical operators in javascript

---
+++ mathopt.js
@@ -0,0 +1,13 @@
+// Math operators in JavaScript
+
+var currentYear = 2020;
+var birthYear = 1965;
+var age = currentYear - birthYear;
+
+var monthlySalary = 78000;
+var annualSalary = monthlySalary * 12;
+
+alert("My dad's age is " + age + " annual salary is Rs. " + annualSalary);
+// Similarly we have + , / , % etc
+
+// Logical operators in JavaScript
