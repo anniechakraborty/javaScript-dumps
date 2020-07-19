d7319b0819582de80b716407059da030e0566bb1
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-07-19 21:06:39 +0530

    logical operator and typeof operator

--- mathopt.js
+++ mathopt.js
@@ -11,3 +11,16 @@ alert("My dad's age is " + age + " annual salary is Rs. " + annualSalary);
 // Similarly we have + , / , % etc
 
 // Logical operators in JavaScript
+
+var boo = 5 > 2;
+console.log(boo);
+
+// typeof operator
+// returns the type of th evalue stored by the variable
+
+console.log(typeof boo);
+console.log(typeof "My name is Khan");
+console.log(typeof currentYear);
+console.log(typeof monthlySalary);
+var xx;
+console.log(typeof xx);
